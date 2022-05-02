a853158c02f6301044308c6dfea3247a1ee2e9e7
yuujinkim <yuuuuu01@naver.com>
2022-05-02 10:08:59 +0900

    0407 django_model

---
+++ likelion/project/blog/models.py
@@ -0,0 +1,13 @@
+from operator import mod
+from django.db import models
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    author = models.CharField(max_length=5, null=True)
+    email = models.EmailField(default='')
+
+    def __str__(self):
+        return self.title
